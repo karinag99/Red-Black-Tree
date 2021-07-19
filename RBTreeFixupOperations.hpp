18c1b6b69f589b67ec77a730a4dc49d6bfbd87c7
karinag99 <karinageorgieva99@gmail.com>
2021-07-19 14:15:46 +0300

    Reinitialization of parent_node after each execution of the while-loop fixed

--- RBTreeFixupOperations.hpp
+++ RBTreeFixupOperations.hpp
@@ -1,5 +1,5 @@
-#ifndef RBTREE_BASE
-#define RBTREE_BASE
+#ifndef _RBTREE_FIXUP_
+#define _RBTREE_FIXUP_
 
 #include "RBTreeMemoryManager.hpp"
 
@@ -62,26 +62,26 @@ protected:
             parents_sibling = parent_node->is_left_child() ? parent_node->parent->right : 
                                                           parent_node->parent->left;
 
+            //case 1
             if(parents_sibling->is_red())
             {
                fix_parent_color(parents_sibling, parent_node);
                new_node = new_node->parent->parent;
+               parent_node = new_node->parent;
             }
             else
             {
                 if(parent_node->is_left_child())
                 {
-                    ensure_new_node_is_left_child(new_node, parent_node);
-                    make_parent_node_black(parent_node, rotateRight);
+                    ensure_new_node_is_left_child(new_node, parent_node); //case 2
+                    make_parent_node_black(parent_node, rotateRight); //case 3
                 }
                 else
                 {
-                    ensure_new_node_is_right_child(new_node, parent_node);
-                    make_parent_node_black(parent_node, rotateLeft);
+                    ensure_new_node_is_right_child(new_node, parent_node); //case 2
+                    make_parent_node_black(parent_node, rotateLeft); // case 3
                 }
             }
-
-            parent_node = new_node->parent;
         }
 
         root->make_black();
@@ -90,26 +90,20 @@ protected:
 private:
 //insert_fixup helper functions
 
-    void make_parent_node_black(node_ptr& parent_node, rotation_ptr rotation)
-    {
-        parent_node->make_black();
-        parent_node->parent->make_red();
-        (this->*rotation)(parent_node->parent);
-    }
-
     void fix_parent_color(node_ptr& parents_sibling, node_ptr& parent_node)
     {
         parents_sibling->make_black();
         parent_node->make_black();
         parent_node->parent->make_red();
     }
-
+    
     void ensure_new_node_is_left_child(node_ptr& new_node, node_ptr& parent_node)
     {
         if(new_node == parent_node->right)
         {
             new_node = parent_node;
             rotateLeft(new_node);
+            parent_node = new_node->parent;
         }
     }
 
@@ -119,9 +113,17 @@ private:
         {
             new_node = parent_node;
             rotateRight(new_node);
+            parent_node = new_node->parent;
         }
     }
 
+    void make_parent_node_black(node_ptr& parent_node, rotation_ptr rotation)
+    {
+        parent_node->make_black();
+        parent_node->parent->make_red();
+        (this->*rotation)(parent_node->parent);
+    }
+   
 protected:
     void delete_fixup(node_ptr fixup_node)
     {
@@ -148,28 +150,6 @@ protected:
 
 private:
 //delete_fixup helper functions
-
-    void make_sibling_black(node_ptr& sibling, node_ptr& parent_node, rotation_ptr rotation)
-    {
-        sibling->make_black();
-        parent_node->make_red();
-        (this->*rotation)(parent_node);
-    }
-  
-    void swap_childrens_colors(node_ptr& sibling, node_ptr& redChild, rotation_ptr rotation)
-    {
-        redChild->make_black();
-        sibling->make_red();
-        (this->*rotation)(sibling);
-    }
-
-    void compensate_doubly_node(node_ptr& sibling, node_ptr& parent_node, node_ptr& redChild, rotation_ptr rotation)
-        {
-            sibling->color = parent_node->color;
-            parent_node->make_black();
-            redChild->make_black();
-            (this->*rotation)(parent_node);
-        }
 
     void fixup_helper_left(node_ptr& fixup_node, node_ptr& sibling, node_ptr& fixup_parent)
     {
@@ -222,5 +202,28 @@ private:
             fixup_node = root;
         }
     }
+
+    void make_sibling_black(node_ptr& sibling, node_ptr& parent_node, rotation_ptr rotation)
+    {
+        sibling->make_black();
+        parent_node->make_red();
+        (this->*rotation)(parent_node);
+    }
+  
+    void swap_childrens_colors(node_ptr& sibling, node_ptr& redChild, rotation_ptr rotation)
+    {
+        redChild->make_black();
+        sibling->make_red();
+        (this->*rotation)(sibling);
+    }
+
+    void compensate_doubly_node(node_ptr& sibling, node_ptr& parent_node, node_ptr& redChild, rotation_ptr rotation)
+        {
+            sibling->color = parent_node->color;
+            parent_node->make_black();
+            redChild->make_black();
+            (this->*rotation)(parent_node);
+        }
+
 };
 #endif
